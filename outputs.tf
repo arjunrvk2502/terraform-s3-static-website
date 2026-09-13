01088e4d82d0e8644150e4beda7753f8380a61d9
arjunrvk2502 <arjunrvk2502@gmail.com>
2026-09-13 18:56:36 +0530

    Initial Commit

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "websiteendpoint" {
+  value = aws_s3_bucket.my_s3_bucket.website_endpoint
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "bucketname" {
+  description = "Name of the S3 bucket"
+  type = string
+  default = "arjunrvk-portfolio-website-bucket"
+}
